dc34ed8a2fb69b20aa7ad8dba9eccb545f784e06
Doak Covington <doakcovington@protonmail.com>
2019-12-22 22:02:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,9 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
 # Whatever you say to grandma, she should respond with
+  if string.upcase != string
+    return "HUH?! SPEAK UP SONNY!"
+  end
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
