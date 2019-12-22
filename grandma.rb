9a320420aa72a72a403d2eb39e10ec969e33fc86
Doak Covington <doakcovington@protonmail.com>
2019-12-22 22:05:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,6 +5,8 @@ def speak_to_grandma(string)
     return "HUH?! SPEAK UP, SONNY!"
   elsif string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  else
+    return "NO, NOT SINCE 1938!"
   end
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
